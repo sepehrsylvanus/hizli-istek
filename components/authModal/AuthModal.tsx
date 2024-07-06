2299530528cc63e08b0ec8e6b464619aa0321f86
Sylvanus <sepehr.haghdoust@gmail.com>
2024-07-06 12:04:29 +0330

    phone number

--- components/authModal/AuthModal.tsx
+++ components/authModal/AuthModal.tsx
@@ -17,28 +17,52 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 import { useGetCountries } from "@/hooks/useGetCountries";
+import Image from "next/image";
+import { FaAngleDown } from "react-icons/fa";
+import { Separator } from "../ui/separator";
 
+type Code = {
+  image: string;
+  code: string;
+  alt: string;
+};
 const formSchema = z.object({
-  username: z.string().min(2).max(50),
+  code: z.string(),
+  phoneNumber: z.string(),
 });
 
 // ============= MAIN COMPONENT ============
 const AuthModal = () => {
   const [step, setStep] = useState("phone");
-  const [codes, setCodes] = useState<String[]>([]);
+  const [codes, setCodes] = useState<Code[]>([]);
   //   ============ FORM CONFIGS============
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      code: "+357",
+      phoneNumber: "",
     },
   });
+  const codeVal = form.watch("code");
+  console.log(codeVal);
+  const buttonThings: Code[] = codes.filter((item) => item.code === codeVal);
+  console.log(buttonThings);
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
@@ -50,10 +74,24 @@ const AuthModal = () => {
 
   const { data: countries, isLoading: isCountriesLoading } = useGetCountries();
   console.log(countries);
+  const standardCountries = countries?.filter(
+    (country) =>
+      country.name.common === "Iran" ||
+      country.name.common === "Cyprus" ||
+      country.name.common === "Turkey"
+  );
+  console.log(standardCountries);
   // ============== END OF CUSTOM HOOKS ============
   useEffect(() => {
-    countries?.forEach((country) => {
-      const code = `${country.idd.root}${country.idd.suffixes}`;
+    standardCountries?.forEach((country) => {
+      console.log(country.flags.svg);
+      console.log(country.idd);
+
+      const code = {
+        image: country.flags.svg,
+        code: `${country.idd.root}${country.idd.suffixes}`,
+        alt: country.flags.alt,
+      };
       setCodes((prev) => [...prev, code]);
     });
   }, [countries]);
@@ -67,29 +105,93 @@ const AuthModal = () => {
       <DialogTrigger className="bg-white mr-[4.6em] py-3 px-[1.75em] rounded-xl hover:text-onColor transition hover:bg-primary">
         Login
       </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className="text-center">Login / Register</DialogTitle>
+      <DialogContent className="h-[522px] x-[522px] flex flex-col pb-[3em]">
+        <DialogHeader className="h-fit">
+          <DialogTitle className="text-center text-[20px]">
+            Login / Register
+          </DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="mt-[4em] flex flex-col justify-between h-full"
+          >
             <FormField
               control={form.control}
-              name="username"
+              name="phoneNumber"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Add your phone number</FormLabel>
+                  <FormLabel className="text-[18px] font-normal">
+                    Add your phone number
+                  </FormLabel>
                   <FormControl>
-                    <Input placeholder="shadcn" {...field} />
+                    <div className=" border border-primary rounded-xl flex items-center px-2 h-[56px]">
+                      <DropdownMenu>
+                        <DropdownMenuTrigger className="flex items-center">
+                          <Button className="bg-transparent hover:bg-transparent outline-none text-[18px]">
+                            <Image
+                              src={buttonThings[0].image}
+                              alt={buttonThings[0].alt}
+                              width={24}
+                              height={24}
+                              className="mr-2"
+                            />
+                            {buttonThings[0].code}
+                            <FaAngleDown />
+                            <Separator
+                              orientation="vertical"
+                              className="mx-4"
+                            />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent className="w-56">
+                          <Controller
+                            name="code"
+                            control={form.control}
+                            render={({ field }) => (
+                              <DropdownMenuRadioGroup
+                                value={field.value}
+                                onValueChange={field.onChange}
+                              >
+                                {codes.map((code, index) => (
+                                  <DropdownMenuRadioItem
+                                    key={index}
+                                    value={code.code}
+                                    className="text-[18px]"
+                                  >
+                                    <Image
+                                      src={code.image}
+                                      alt={code.alt}
+                                      width={24}
+                                      height={24}
+                                      className="mr-2"
+                                    />
+                                    {code.code}
+                                  </DropdownMenuRadioItem>
+                                ))}
+                              </DropdownMenuRadioGroup>
+                            )}
+                          />
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                      <Input
+                        placeholder="shadcn"
+                        {...field}
+                        className=" border-none outline-none rounded-xl text-[18px] bg-transparent"
+                      />
+                    </div>
                   </FormControl>
-                  <FormDescription>
-                    This is your public display name.
-                  </FormDescription>
+
                   <FormMessage />
                 </FormItem>
               )}
             />
-            <Button type="submit">Submit</Button>
+            <Button
+              type="submit"
+              className="text-onColor  mx-auto  text-[24px] font-light w-[192px] h-[64px] rounded-2xl"
+            >
+              Next
+            </Button>
           </form>
         </Form>
       </DialogContent>
--- types/types.d.ts
+++ types/types.d.ts
@@ -1,6 +1,13 @@
 type country = {
+  name: {
+    common: string;
+  };
   idd: {
     root: string;
     suffixes: string;
+  };
+  flags: {
+    svg: string;
+    alt: string;
   };
 };
